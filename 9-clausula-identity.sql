c4a3c6a6cbe605a4dd87b3f2cd0388d9b21d7801
Matheus Santos <81287841+whymat@users.noreply.github.com>
2023-09-19 22:39:06 -0300

    Delete 9-clausula-identity.sql

--- 9-clausula-identity.sql
+++
@@ -1,7 +0,0 @@
--- IDENTITY (<início>, <incremento>)
-
-CREATE TABLE Cargo (
-	idCargo INT IDENTITY PRIMARY KEY, -- auto-incremento, não é necessário ficar adicionando manualmente os ID
-	sigla CHAR(2) NOT NULL,
-	nome VARCHAR(30) NULL UNIQUE
-)
